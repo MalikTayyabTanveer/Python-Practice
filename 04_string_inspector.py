fe78d21809d63b495d8f517a416afe3ef23381b6
Malik Tayyab Tanveer <138617303+MalikTayyabTanveer@users.noreply.github.com>
2025-07-20 15:46:46 +0500

    added the checking method

--- 04_string_inspector.py
+++ 04_string_inspector.py
@@ -7,4 +7,7 @@ print(f"the first character of the string is {sentence[0]}")
 print(f"the last character of the string is {sentence[-1]}")
 print(f"the upper version of the sentence is {sentence.upper()}")
 print(f"the lower version of the sentence is {sentence.lower()}")
-print(f"the 'a' in the sentence occures {sentence.count('a')} times")
+if "a" in sentence:
+    print(f"the 'a' in the sentence occures {sentence.count('a')} times")
+else:
+    print("there is no 'a' in the strig")
