9c25c451243dbdb05e90ee4e88d702d9656eb8b1
Malik Tayyab Tanveer <138617303+MalikTayyabTanveer@users.noreply.github.com>
2025-07-20 15:43:46 +0500

    basics practice

---
+++ 04_string_inspector.py
@@ -0,0 +1,10 @@
+"""Take a word from user. Display: length, first character, last character, uppercase version,
+lowercase version, and check if it contains the letter 'a'."""
+
+sentence = input("enter your sentence: ")
+print(f"lenght of the string is {len(sentence)}")
+print(f"the first character of the string is {sentence[0]}")
+print(f"the last character of the string is {sentence[-1]}")
+print(f"the upper version of the sentence is {sentence.upper()}")
+print(f"the lower version of the sentence is {sentence.lower()}")
+print(f"the 'a' in the sentence occures {sentence.count('a')} times")
